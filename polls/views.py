e4104d5afb2e5209d5491af0c4c62e2918f44362
ryo.kaneoka0406@gmail.com <ryo.kaneoka0406@gmail.com>
2020-01-08 07:46:26 +0900

    update views&urls

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,15 @@
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Hello, World. You're at the polls index")
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at qestion %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of qestion %s." 
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on qestion %s." % question_id)
