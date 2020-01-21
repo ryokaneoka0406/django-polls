ec2f99e6600f451764c1032a14e1e0be8cb1cfe7
ryo.kaneoka0406@gmail.com <ryo.kaneoka0406@gmail.com>
2020-01-22 08:18:05 +0900

    add test again(old question and recent one)

--- polls/views.py
+++ polls/views.py
@@ -24,7 +24,6 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
-def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
@@ -37,3 +36,4 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+def vote(request, question_id):
