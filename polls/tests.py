5c7b1feb0db40a12b7033a30318dc7facd32bdfd
ryo.kaneoka0406@gmail.com <ryo.kaneoka0406@gmail.com>
2020-01-24 06:29:15 +0900

    shape the test code

--- polls/tests.py
+++ polls/tests.py
@@ -5,19 +5,22 @@ from django.utils import timezone
 
 from .models import Question
 
+
 class QuestionModelTests(TestCase):
     def test_was_published_recently_future_question(self):
         """
-        was_published_recently() returns False for questions whose pub_date is in the future
+        was_published_recently() returns False for questions
+        whose pub_date is in the future
         """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
-        
+
         self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_old_question(self):
         """
-        was_published_recently() returns False for questions whose pub_date is older than 1day
+        was_published_recently() returns False for questions
+        whose pub_date is older than 1day
         """
 
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
@@ -27,9 +30,12 @@ class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_recent_question(self):
         """
-        was_published_recently() returns True for questions whose pub_date is within the last day
+        was_published_recently() returns True for questions
+        whose pub_date is within the last day
         """
-        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        time = timezone.now() - datetime.timedelta(
+            hours=23, minutes=59, seconds=59
+        )
         recent_question = Question(pub_date=time)
 
         self.assertIs(recent_question.was_published_recently(), True)
