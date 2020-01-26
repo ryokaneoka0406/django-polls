0a45393c780a11d4c84a09aa88749dbc858713ba
ryo.kaneoka0406@gmail.com <ryo.kaneoka0406@gmail.com>
2020-01-26 23:08:38 +0900

    add test

--- polls/tests.py
+++ polls/tests.py
@@ -73,3 +73,25 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question.>']
         )
+
+    def test_future_question(self):
+        """
+        Question with a pub_date in the future aren't displayed on the index
+        page.
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        displayed
+        """
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question>']
+        )
